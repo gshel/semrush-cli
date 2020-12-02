ad511d7b9fa98763cde66f0d5d8c64285d220213
Gretchen Shelby-Dormer <35184207+gshel@users.noreply.github.com>
2020-12-02 13:00:54 -0500

    Fix issue 2 missing version file (#14)
    * Reimplement building a wheel for packaging
    
    * Include VERSION file in package
    
    * Fix broken path to VERSION file

--- semrush/__init__.py
+++ semrush/__init__.py
@@ -1,4 +1,5 @@
-#import os
-#import click
+import os
+import click
 
-#__version__ = open(os.path.join(".", "VERSION")).read().strip() # TODO figure out how to include version at build time because pypi wheels do not include non-code files like VERSION
+
+__version__ = open(os.path.join("..", "VERSION")).read().strip()
